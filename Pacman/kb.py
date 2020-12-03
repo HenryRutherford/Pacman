631a1ba7fec957ffa7c4b10c78e666ad793362ee
kharanga <60679652+kharanga@users.noreply.github.com>
2020-12-02 20:14:14 -0800

    Add files via upload

--- Pacman/kb.py
+++ Pacman/kb.py
@@ -20,8 +20,8 @@ class PropKB(KB):
     #3 = no fodd & ghost
     #4 = food & ghost
     #Every coordinates start off as 1
-    def __init__ (self, height, width):
-        self.array = [[1 for i in range(height)]for j in range(width)]
+    def __init__ (self, x, y):
+        self.array = [[1 for i in range(x)]for j in range(y)]
 
     def tell(self, x, y, state):
         self.array[x][y] = state
@@ -31,21 +31,70 @@ class PropKB(KB):
 
     def edit(self, x, y, state):
         if state == 0:
-            print("Successfully change from ", self.ask(x, y), " to ", state)
+            if(self.ask(x, y) == 1):
+                print("Successfully change from having food to empty")
+            if (self.ask(x, y) == 2):
+                print("Successfully change from having pacman to empty")
+            if (self.ask(x, y) == 3):
+                print("Successfully change from having no food and a ghost to empty")
+            if (self.ask(x, y) == 4):
+                print("Successfully change from having food and a ghost to empty")
             self.tell(x, y, state)
         if state == 1:
-            if self.ask(x, y) == 4 or self.ask(x, y) == 1:
-                print("Successfully change from ", self.ask(x, y), " to ", state)
+            if self.ask(x, y) == 4:
+                print("Successfully change from having food and a ghost to having food")
                 self.tell(x, y, state)
+            else:
+                if (self.ask(x, y) == 0):
+                    print("Unsuccessfully change from empty to having food")
+                if (self.ask(x, y) == 2):
+                    print("Unsuccessfully change from having pacman to having food")
+                if (self.ask(x, y) == 3):
+                    print("Unsuccessfully change from having no food and a ghost to having food")
         if state == 2:
-            print("Successfully change from ", self.ask(x, y), " to ", state)
+            if (self.ask(x, y) == 0):
+                print("Successfully change from empty to having pacman")
+            if (self.ask(x, y) == 1):
+                print("Successfully change from having food to having pacman")
+            if (self.ask(x, y) == 3):
+                print("Successfully change from having no food and a ghost to having pacman")
+            if (self.ask(x, y) == 4):
+                print("Successfully change from having food and a ghost to having pacman")
             self.tell(x, y, state)
         if state == 3:
-            print("Successfully change from ", self.ask(x, y), " to ", state)
+            if (self.ask(x, y) == 0):
+                print("Successfully change from empty to having no food and a ghost")
+            if (self.ask(x, y) == 1):
+                print("Successfully change from having food to having no food and a ghost")
+            if (self.ask(x, y) == 2):
+                print("Successfully change from having pacman to having no food and a ghost")
+            if (self.ask(x, y) == 4):
+                print("Successfully change from having food and a ghost to having no food and a ghost")
             self.tell(x, y, state)
         if state == 4:
-            if self.ask(x, y) == 4 or self.ask(x, y) == 1:
-                print("Successfully change from ", self.ask(x, y), " to ", state)
+            if self.ask(x, y) == 1:
+                print("Successfully change from having food to have a ghost and food")
                 self.tell(x, y, state)
-    def print_kb(self):
-        print (self.array)
+            else:
+                if (self.ask(x, y) == 0):
+                    print("Unsuccessfully change from empty to having food and a ghost")
+                if (self.ask(x, y) == 2):
+                    print("Unsuccessfully change from having pacman to having food having food and a ghost")
+                if (self.ask(x, y) == 3):
+                    print("Unsuccessfully change from having no food and a ghost to having food and a ghost")
+    def print_kb(self, x, y):
+        for j in range(x):
+            for i in range(y):
+                print("[", end = "")
+                if self.array[i][j] == 0:
+                    print("    empty   ", end = "")
+                if self.array[i][j] == 1:
+                    print("    food    ", end = "")
+                if self.array[i][j] == 2:
+                    print("   pacman   ", end = "")
+                if self.array[i][j] == 3:
+                    print("    ghost   ", end = "")
+                if self.array[i][j] == 4:
+                    print("ghost & food", end = "")
+                print("]", end = "")
+            print("\n")
--- Pacman/kb_driver.py
+++ Pacman/kb_driver.py
@@ -2,11 +2,16 @@ from kb import *
 x = 3
 y = 4
 kb = PropKB(x, y)
-print("KB is initialized, every square is 1")
-kb. edit (1, 2, 0)
-kb. edit (1, 2, 3)
-kb. edit (2, 1, 3)
-kb. edit (2, 1, 4)
-kb.print_kb()
+print("KB is initialized, every square have food")
+#Our kb going to change everytime the pacman or the four ghost move, thus will change the board.
+#These informations will be take into considerate when our agent deciding its next move
+kb.edit (1, 2, 0)
+kb.edit (1, 2, 3)
+kb.edit (2, 1, 3)
+kb.edit (2, 1, 4)
+kb.edit (3, 2, 2)
+kb.edit (0, 2, 4)
+kb.edit (0, 0, 0)
+kb.print_kb(x, y)
 
 
